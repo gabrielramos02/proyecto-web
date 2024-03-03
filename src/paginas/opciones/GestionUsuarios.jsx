9e3554abbd387e74b6ff6f342e3b00e3251eaeb1
Gabriel Ramos <109390599+gabrielramos02@users.noreply.github.com>
2024-03-03 16:49:30 -0500

    added  columna rol

--- src/paginas/opciones/GestionUsuarios.jsx
+++ src/paginas/opciones/GestionUsuarios.jsx
@@ -1,15 +1,17 @@
 import CrearUsuario from "../../components/CrearUsuario"
 import ModificarUsuario from "../../components/ModificarUsuario"
 import EliminarUsuario from "../../components/EliminarUsuario"
+import { useState } from "react"
 const GestionUsuarios = () => {
+    const[cargar,SetCargar]=useState(false)
     return (
         <div>
             <h1 className="font-black text-3xl text-center">
                 Gestion de Usuario
             </h1>
             <div className="lg:flex lg:justify-center mt-3">
-                <CrearUsuario />
-                <ModificarUsuario />
+                <CrearUsuario value={[cargar,SetCargar]}/>
+                <ModificarUsuario value={[cargar,SetCargar]}/>
             </div>
         </div>
     )
