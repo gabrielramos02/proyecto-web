d862fd38d524c4d8837542f66b0106144d996153
Gabriel Ramos <109390599+gabrielramos02@users.noreply.github.com>
2024-03-16 23:16:10 -0400

    added lista operaciones del dia

--- src/paginas/Director.jsx
+++ src/paginas/Director.jsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react"
+import ListaOperaciones from "../components/ListaOperaciones"
+import clienteAxios from "../config/clienteAxios"
+
 const Director = () => {
-  return (
-    <div>
-      <h1 className="text-4xl font-black">Pagina principal</h1>
-      
-    </div>
-  )
+    const [operaciones, setOperaciones] = useState([])
+
+    useEffect(() => {
+        const getOperaciones = async () => {
+            const access_token = localStorage.getItem("access_token")
+            try {
+                const { data } = await clienteAxios(
+                    "/operacion/operacionesplanificadas/all",
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${access_token}`,
+                        },
+                    }
+                )
+                setOperaciones(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getOperaciones()
+    }, [])
+
+    return (
+        <div>
+            <h1 className="text-4xl font-black">Pagina principal</h1>
+            <ListaOperaciones
+                key={operaciones.length}
+                operaciones={operaciones}
+                setOperaciones={setOperaciones}
+            />
+        </div>
+    )
 }
 export default Director
