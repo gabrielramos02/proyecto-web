e8c23005e2222b31314c3f3f1cfee7d1c903e545
Gabriel Ramos <109390599+gabrielramos02@users.noreply.github.com>
2024-03-25 11:19:54 -0400

    added estadisticas

--- src/paginas/Director.jsx
+++ src/paginas/Director.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react"
 import ListaOperaciones from "../components/ListaOperaciones"
 import clienteAxios from "../config/clienteAxios"
 import ListaOperacionesUrgencias from "../components/ListaOperacionesUrgencias"
+import Ocupacion from "../components/Ocupacion"
 
 const Director = () => {
     const [operaciones, setOperaciones] = useState([])
     const [operacionesUrgencia, setOperacionesUrgencia] = useState([])
+    const [camasSalas,setCamasSalas] = useState([])
 
     useEffect(() => {
         const getOperaciones = async () => {
@@ -44,21 +46,41 @@ const Director = () => {
             }
         }
         getOperacionesUrgencia()
+        const getCamasSalas = async () => {
+          const access_token = localStorage.getItem("access_token")
+          try {
+              const { data } = await clienteAxios("/ocupacion", {
+                  headers: {
+                      "Content-Type": "application/json",
+                      Authorization: `Bearer ${access_token}`,
+                  },
+              })
+              // data = data.filter(cama => cama)
+              setCamasSalas(data)
+          } catch (error) {
+              setAlert({
+                  msg: error.response.data.detail,
+                  error: true,
+              })
+          }
+      }
+      getCamasSalas()
     }, [])
 
     return (
         <div>
             <h1 className="text-4xl font-black">Pagina principal</h1>
             <ListaOperaciones
-                key={operaciones.length}
+                key={"operaciones"}
                 operaciones={operaciones}
                 setOperaciones={setOperaciones}
             />
             <ListaOperacionesUrgencias
-                key={operacionesUrgencia.length}
+                key={"urgencia"}
                 operacionesUrgencia={operacionesUrgencia}
                 setOperacionesUrgencia={setOperacionesUrgencia}
             />
+            <Ocupacion camasSalas={camasSalas} setCamasSala={setCamasSalas} />
         </div>
     )
 }
